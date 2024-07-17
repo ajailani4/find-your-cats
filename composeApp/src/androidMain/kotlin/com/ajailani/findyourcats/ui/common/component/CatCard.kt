18cb2d6fe0a21b412663c997f25b6067906423f2
Ahmad Jailani <ahmadjailani604@gmail.com>
2024-07-17 23:31:24 +0700

    Remove unused import

--- composeApp/src/androidMain/kotlin/com/ajailani/findyourcats/ui/common/component/CatCard.kt
+++ composeApp/src/androidMain/kotlin/com/ajailani/findyourcats/ui/common/component/CatCard.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Eco
-import androidx.compose.material.icons.filled.Grass
 import androidx.compose.material.icons.filled.Straighten
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
